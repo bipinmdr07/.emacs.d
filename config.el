aa1f7af9f7f6c192c06290138f57421cf1a29e8f
Bipin Manandhar <kubipin07@gmail.com>
2025-10-08 15:54:53 +0545

    Configure EVIL mode to have vim key bindings and enable it globally.

--- config.el
+++ config.el
@@ -36,3 +36,50 @@
     (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
+
+;; Ensure exec-path-from-shell is installed and initialized early for native compilation
+(elpaca exec-path-from-shell
+  (use-package exec-path-from-shell
+    :init
+    (when (memq window-system '(mac ns x))
+      (exec-path-from-shell-copy-envs
+       '("PATH" "LIBRARY_PATH" "CPATH" "PKG_CONFIG_PATH")))))
+
+;; Install a package via the elpaca macro
+;; See the "recipes" section of the manual for more details.
+
+;; (elpaca example-package)
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
+
+;;When installing a package used in the init file itself,
+;;e.g. a package which adds a use-package key word,
+;;use the :wait recipe keyword to block until that package is installed/configured.
+;;For example:
+;;(use-package general :ensure (:wait t) :demand t)
+
+;; Expands to: (elpaca evil (use-package evil :demand t))
+(use-package evil
+  :ensure t
+:demand t
+:init
+(setq evil-want-integration t
+      evil-want-keybinding nil)
+:config
+(evil-mode 1)) ;; Globally enabling evil mode
+
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (setq evilcollection-mode-list '(dashboard dired ibuffer))
+  (evil-collection-init))
+
+;;Turns off elpaca-use-package-mode current declaration
+;;Note this will cause evaluate the declaration immediately. It is not deferred.
+;;Useful for configuring built-in emacs features.
+(use-package emacs :ensure nil :config (setq ring-bell-function #'ignore))
