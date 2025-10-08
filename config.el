4044e9a34a1ec3c56bb03b0b50b8a8b924479f6c
Bipin Manandhar <kubipin07@gmail.com>
2025-10-08 16:29:10 +0545

    Add doom themes, and set doom-one as default theme.

--- config.el
+++ config.el
@@ -132,6 +132,26 @@
 
   )
 
+(use-package doom-themes
+  :ensure t
+  :custom
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
+  ;; for treemacs users
+  (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  :config
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
 (use-package which-key
   :init
     (which-key-mode 1)
