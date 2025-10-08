247a7fd717cc9b0916fdb57fd93b5c95ea3654af
Bipin Manandhar <kubipin07@gmail.com>
2025-10-08 20:33:05 +0545

    Add company mode for autocompletion.

--- config.el
+++ config.el
@@ -221,6 +221,24 @@
 ;;   :ensure t
 ;;   :init (all-the-icons-ivy-rich-mode 1))
 
+(use-package company
+   :ensure t
+   :defer 2
+   :diminish
+   :custom
+   (company-begin-command '(self-insert-command))
+   (company-idle-delay .1)
+   (company-minimum-prefix-length 2)
+   (company-show-numbers t)
+   (company-tooltip-align-annotations 't)
+   (global-company-mode t))
+
+ (use-package company-box
+:ensure t
+   :after company
+   :diminish
+   :hook (company-mode . company-box-mode))
+
 (use-package general
   :ensure t
   :config
